b382bb6823a63856adf4ad31941c7f5050c0145a
Dominik <github@dominik-schmitt.eu>
2017-08-03 20:11:54 +0200

    Moved public files for static serve

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var logger = require('winston');
 logger.info('Im fine');
-const app = require('express')();
+const path = require('path');
+const express = require('express')
+const app = express();
 const routes = require('./routes');
 const PORT = process.env.PORT || 3000;
 
 //  Connect all our routes to our application
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/', routes);
 
 // Turn on that server!
