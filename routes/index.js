b7f6f610a0e4e02e4a71549f82a545dc95080e55
Dominik <github@dominik-schmitt.eu>
2017-08-14 14:21:13 +0200

    Added sqlite3 support for later job work

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var routes = require('express').Router();
 var logger = require('winston');
 var cronjobs = require('../scripts/cronjob.js');
+var databse = require('../scripts/database.js');
 
 routes.get('/', (req, res) => {
   logger.info('Got Request on API v1');
